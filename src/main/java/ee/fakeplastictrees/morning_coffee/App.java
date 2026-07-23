7fe59aaa0d678731f9972dfb617ed111b0efd35a
Aleksei Parfentjev <aleksei@fakeplastictrees.ee>
2026-07-23 23:29:35 +0300

    add Config class

--- src/main/java/ee/fakeplastictrees/morning_coffee/App.java
+++ src/main/java/ee/fakeplastictrees/morning_coffee/App.java
@@ -4,6 +4,8 @@ import java.util.concurrent.Executors;
 
 public class App {
   public static void main(String[] args) throws Exception {
+    var config = new Config();
+
     var repository = new Repository();
 
     var readerExecutor = Executors.newSingleThreadScheduledExecutor();
