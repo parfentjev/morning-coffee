62f1d1773bb88d9a94a4d4467157ed918ff632fa
Aleksei Parfentjev <aleksei@fakeplastictrees.ee>
2026-07-31 20:33:37 +0300

    fix warnings and errors, pt. 2

--- spec/schema.sql
+++ spec/schema.sql
@@ -1,19 +1,21 @@
-CREATE TABLE public.feeds (
-	id uuid DEFAULT uuidv7() NOT NULL,
-	name text NOT NULL,
-	url text NOT NULL,
-	enabled boolean DEFAULT TRUE NOT NULL,
-	CONSTRAINT feeds_pkey PRIMARY KEY (id)
+CREATE TABLE public.feeds
+(
+    id      uuid    DEFAULT uuidv7() NOT NULL,
+    name    text                     NOT NULL,
+    url     text                     NOT NULL,
+    enabled boolean DEFAULT TRUE     NOT NULL,
+    CONSTRAINT feeds_pkey PRIMARY KEY (id)
 );
 
-CREATE TABLE public.entries (
-	id uuid DEFAULT uuidv7() NOT NULL,
-	external_id text NOT NULL,
-	published_at timestamptz NOT NULL,
-	feed_id uuid NOT NULL,
-	title text NOT NULL,
-	link text NOT NULL,
-	CONSTRAINT entries_pkey PRIMARY KEY (id),
-	CONSTRAINT entries_feed_id_fkey FOREIGN KEY (feed_id) REFERENCES public.feeds(id),
-	CONSTRAINT entries_external_id_feed_id_key UNIQUE (external_id, feed_id)
+CREATE TABLE public.entries
+(
+    id           uuid DEFAULT uuidv7() NOT NULL,
+    external_id  text                  NOT NULL,
+    published_at timestamptz           NOT NULL,
+    feed_id      uuid                  NOT NULL,
+    title        text                  NOT NULL,
+    link         text                  NOT NULL,
+    CONSTRAINT entries_pkey PRIMARY KEY (id),
+    CONSTRAINT entries_feed_id_fkey FOREIGN KEY (feed_id) REFERENCES public.feeds (id),
+    CONSTRAINT entries_external_id_feed_id_key UNIQUE (external_id, feed_id)
 );
