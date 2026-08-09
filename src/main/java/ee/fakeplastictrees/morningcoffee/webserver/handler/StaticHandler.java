27fbc8e60478ee18611aff45fbe02f62e6e1dffa
Aleksei Parfentjev <aleksei@fakeplastictrees.ee>
2026-08-09 23:32:10 +0300

    define todo list for StaticHandler

---
+++ src/main/java/ee/fakeplastictrees/morningcoffee/webserver/handler/StaticHandler.java
@@ -0,0 +1,29 @@
+package ee.fakeplastictrees.morningcoffee.webserver.handler;
+
+public class StaticHandler extends AbstractHttpHandler {
+  // TODO:
+  // 1. Implement this handler to serve static files from resources.
+  // 2. Add basic CSS.
+  // 3. Add jk bindings using JS.
+  // 4. Consider security risks.
+  // 5. Validate how HttpServer contexts work. Do I even need requestPath? Or does context serve
+  // only its path and this check is redundant?
+
+  @Override
+  protected String requestMethod() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'requestMethod'");
+  }
+
+  @Override
+  protected String requestPath() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'requestPath'");
+  }
+
+  @Override
+  protected Response response() throws Exception {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'response'");
+  }
+}
